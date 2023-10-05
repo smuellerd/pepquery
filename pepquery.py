360e342596fe7cc38d5f7a6032df94d70911ab03
Sophia Müller-Dott <78087387+smuellerd@users.noreply.github.com>
2023-10-05 16:09:42 +0200

    Update and rename lumache.py to pepquery.py

---
+++ pepquery.py
@@ -0,0 +1,11 @@
+"""
+Pepquery - A peptide-centric search engine for MS/MS data analysis.
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
+
